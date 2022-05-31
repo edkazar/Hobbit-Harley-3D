b36c6bfbd6c2f528863038661b07636bbdef12da
Emma <ehudroge@gmail.com>
2022-05-31 14:19:12 -0500

    import animations

---
+++ Assets/Characters/Hobbit/Editor/ReverseAnimation.cs
@@ -0,0 +1,56 @@
+//ReverseAnimation.cs
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class ReverseAnimation : Editor
+{
+    public static AnimationClip GetSelectedClip()
+    {
+        var clips = Selection.GetFiltered(typeof(AnimationClip), SelectionMode.Assets);
+        if (clips.Length > 0)
+        {
+            return clips[0] as AnimationClip;
+        }
+        return null;
+    }
+    [MenuItem("Tools/ReverseAnimation")]
+    public static void Reverse()
+    {
+        var clip = GetSelectedClip();
+        if (clip == null)
+            return;
+        float clipLength = clip.length;
+        var curves = AnimationUtility.GetAllCurves(clip, true);
+        clip.ClearCurves();
+        foreach (AnimationClipCurveData curve in curves)
+        {
+            var keys = curve.curve.keys;
+            int keyCount = keys.Length;
+            var postWrapmode = curve.curve.postWrapMode;
+            curve.curve.postWrapMode = curve.curve.preWrapMode;
+            curve.curve.preWrapMode = postWrapmode;
+            for (int i = 0; i < keyCount; i++)
+            {
+                Keyframe K = keys[i];
+                K.time = clipLength - K.time;
+                var tmp = -K.inTangent;
+                K.inTangent = -K.outTangent;
+                K.outTangent = tmp;
+                keys[i] = K;
+            }
+            curve.curve.keys = keys;
+            clip.SetCurve(curve.path, curve.type, curve.propertyName, curve.curve);
+        }
+        var events = AnimationUtility.GetAnimationEvents(clip);
+        if (events.Length > 0)
+        {
+            for (int i = 0; i < events.Length; i++)
+            {
+                events[i].time = clipLength - events[i].time;
+            }
+            AnimationUtility.SetAnimationEvents(clip, events);
+        }
+        Debug.Log("Animation reversed!");
+    }
+}
