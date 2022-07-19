7f3a9d3eff1cee57b7e4b9fd4bc23a08424efd3e
edkazar <edtec217@gmail.com>
2022-07-18 18:50:10 -0600

    Head Twitching fixes

--- Assets/Scripts/HeadPosition.cs
+++ Assets/Scripts/HeadPosition.cs
@@ -7,16 +7,26 @@ public class HeadPosition : MonoBehaviour
     [SerializeField] private Transform myPos;
     [SerializeField] Transform camera;
 
+    private Vector3 initialPos;
+
+    [SerializeField]
+    private Transform carTransform;
+
+    [SerializeField] private int rotationResetDistance;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        initialPos = carTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = myPos.position + new Vector3(-0.50f, 0.275f, -0.75f);
-        transform.forward = new Vector3(-camera.forward.x, -transform.forward.y, -camera.forward.z);
+        float distanceZ = Mathf.Abs(initialPos.z - carTransform.position.z);
+        if (distanceZ <= rotationResetDistance)
+        {
+            transform.forward = new Vector3(-camera.forward.x, -transform.forward.y, -camera.forward.z);
+        }
     }
 }
